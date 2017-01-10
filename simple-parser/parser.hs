ee29a2158cc7c4037cac082fc265deeb012f41e9
atungare <ajay.tungare@gmail.com>
2017-01-10 14:30:57 -0500

    parsing: return values

--- simple-parser/parser.hs
+++ simple-parser/parser.hs
@@ -1,6 +1,15 @@
 module Main where
+
+import Control.Monad
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -8,10 +17,43 @@ symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 spaces :: Parser ()
 spaces = skipMany1 space
 
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first:rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _    -> Atom atom
+
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+-- fyi, liftM :: (a -> b) -> m a -> m b
+
+-- parseNumber = do
+--   numString <- many1 digit
+--   return $ (Number . read) numString
+
+-- parseNumber = (many1 digit) >>= (\digits ->
+--               return $ (Number . read) digits)
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom <|> parseString <|> parseNumber
+
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
   Left err -> "No match: " ++ show err
-  Right val -> "Found Value"
+  Right _ -> "Found Value"
 
 main :: IO ()
 main = do
