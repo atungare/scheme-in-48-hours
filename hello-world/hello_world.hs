ce235bae5a70aae5d2ad1d2f7e813e591d712b58
atungare <ajay.tungare@gmail.com>
2017-01-10 12:21:46 -0500

    first steps

---
+++ hello-world/hello_world.hs
@@ -0,0 +1,10 @@
+module Main where
+import System.Environment
+
+main :: IO ()
+main = do
+  putStrLn "Please enter your name: "
+  args <- getArgs
+  name <- getLine
+  putStrLn ("Hello, " ++ name ++ ", this is " ++ (args !! 0) ++ "!")
+  putStrLn ("The sum of " ++ (args !! 1) ++ " and " ++ (args !! 2) ++ " is " ++ show(((read (args !! 1)) + (read (args !! 2)))))
