3f8b019a46cf4a20691718e7ee6576c6ec980108
atungare <ajay.tungare@gmail.com>
2017-01-11 12:14:59 -0500

    parsing: recursive expressions

--- simple-parser/parser.hs
+++ simple-parser/parser.hs
@@ -37,7 +37,6 @@ parseAtom = do
 
 parseNumber :: Parser LispVal
 parseNumber = liftM (Number . read) $ many1 digit
-
 -- fyi, liftM :: (a -> b) -> m a -> m b
 
 -- parseNumber = do
@@ -47,8 +46,30 @@ parseNumber = liftM (Number . read) $ many1 digit
 -- parseNumber = (many1 digit) >>= (\digits ->
 --               return $ (Number . read) digits)
 
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+  head <- endBy parseExpr spaces
+  tail <- char '.' >> spaces >> parseExpr
+  return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+  char '\''
+  x <- parseExpr
+  return $ List [Atom "quote", x]
+
 parseExpr :: Parser LispVal
-parseExpr = parseAtom <|> parseString <|> parseNumber
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
+        <|> parseQuoted
+        <|> do char '('
+               x <- try parseList <|> parseDottedList
+               char ')'
+               return x
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
